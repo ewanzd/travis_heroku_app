5ba66100006454e35132e73447892aaaa78e24b3
Wanzenried <e1700584@edu.vamk.fi>
2018-04-18 13:41:37 +0300

    add travis

--- src/main/java/myapp/Application.java
+++ src/main/java/myapp/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-//@Import (SwaggerConfig.class) //for Swagger documentation - Swagger home http://localhost:8080/swagger-ui.html
+@Import (SwaggerConfig.class) //for Swagger documentation - Swagger home http://localhost:8080/swagger-ui.html
 public class Application {
 
 	public static void main(String[] args) {
