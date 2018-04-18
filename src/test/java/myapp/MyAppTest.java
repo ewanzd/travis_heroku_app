5fef463f4c96e412e81b114a08799ef27724671e
Wanzenried <e1700584@edu.vamk.fi>
2018-04-18 13:34:31 +0300

    add test class

---
+++ src/test/java/myapp/MyAppTest.java
@@ -0,0 +1,36 @@
+package myapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyAppTest {
+    public MyAppTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    //this is a dummy test just testing that the test are run
+    @Test
+    public void hello() {
+        assertEquals(1, 1);
+    }
+}
